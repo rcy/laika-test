569cd682938c1a0d1265400370c7cdb761a69aa9
Ryan Yeske <rcyeske@gmail.com>
2013-09-22 13:27:26 -0700

    remove redundant pub/sub

--- collections/posts.js
+++ collections/posts.js
@@ -15,22 +15,8 @@ if (Meteor.isServer) {
     return Posts.find();
   });
 
-  Meteor.publish('custom-posts', function() {
-    var sub = this;
-    var cursor = Posts.find({}, {fields: {name: 1}});
-    var handle = cursor.observeChanges({
-      added: function(id, doc) {
-        sub.added('posts', id, doc);
-      },
-      // changed: function(id, fields) {
-      //   sub.changed('posts', id, fields);
-      // }
-    });
-    sub.ready();
-  });
 }
 
 if (Meteor.isClient) {
-  Meteor.subscribe('custom-posts');
   Meteor.subscribe('all-posts');
 }
